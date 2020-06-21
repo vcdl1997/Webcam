bb65d719c09ffa782678d6cd2a66a75e8e1a9ddd
Vinicius Costa <vverdao@hotmail.com>
2020-06-21 14:14:43 -0300

    Correção

--- servidor.php
+++ servidor.php
@@ -8,7 +8,7 @@
 				file_put_contents('image.png', $data);
 				// $ftp_server = "files.000webhost.com";
 				// $ftp_user = "crisor";
-				// $ftp_pass = "24031997vT@";
+				// $ftp_pass = "";
 				// $caminho_absoluto = 'public_html/';
 
 				// $conn_id = ftp_ssl_connect($ftp_server) or die("Couldn't connect to {$ftp_server}"); 
